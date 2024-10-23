918c8df7007ad19ee42e31341c51f018ad1029f2
= <=>
2024-10-23 16:18:56 +0600

    Build Auth Pages

---
+++ components/auth-buttons.tsx
@@ -0,0 +1,19 @@
+import { Grid } from "@radix-ui/themes";
+import { FaGithub } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "./ui/button";
+
+const AuthButtons = () => {
+  return (
+    <Grid columns="1fr 1fr" gap="3">
+      <Button variant="outline" className="text-xl">
+        <FcGoogle />
+      </Button>
+      <Button variant="outline" className="text-xl">
+        <FaGithub />
+      </Button>
+    </Grid>
+  );
+};
+
+export default AuthButtons;
