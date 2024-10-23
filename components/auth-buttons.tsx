f3e2dea7cba0adc32cf5c70d1a4321f0078f117b
= <=>
2024-10-23 16:53:28 +0600

    Add OAuth

--- components/auth-buttons.tsx
+++ components/auth-buttons.tsx
@@ -1,12 +1,18 @@
+"use client";
 import { Grid } from "@radix-ui/themes";
 import { FaGithub } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "./ui/button";
+import { signIn } from "next-auth/react";
 
 const AuthButtons = () => {
   return (
     <Grid columns="1fr 1fr" gap="3">
-      <Button variant="outline" className="text-xl">
+      <Button
+        onClick={() => signIn("google", { callbackUrl: "/" })}
+        variant="outline"
+        className="text-xl"
+      >
         <FcGoogle />
       </Button>
       <Button variant="outline" className="text-xl">
